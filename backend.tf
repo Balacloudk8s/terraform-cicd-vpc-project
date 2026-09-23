163b2d605cd5bda6e7f262d2a32bff0cad9fae5b
Balacloudk8s <balacloudk8s@gmail.com>
2026-09-23 06:51:43 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "terraform-s3-backend-prod-tfstate-us"
+    bucket       = "terra-project1-vpc-tfstate"
     key          = "tf-vpc-deployment/vpc.tfstate"
     region       = "us-east-1"
     encrypt      = true    
